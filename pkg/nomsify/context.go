e7612c7c6f582f8f353d5d37a56469024cc8ed14
Eric Schmidt <eric.schmidt@oneiro.io>
2019-06-10 13:47:07 -0700

    Add support for managed vars

--- pkg/nomsify/context.go
+++ pkg/nomsify/context.go
@@ -9,6 +9,7 @@ import (
 	"go/token"
 	"os"
 	"sort"
+	"strings"
 )
 
 type context struct {
@@ -16,6 +17,9 @@ type context struct {
 	file     *ast.File
 	typeNode *ast.TypeSpec
 }
+
+// Fields containing this string in their names are managed variables.
+const managedVarName = "ManagedVar"
 
 // find a node by name and set it
 func (c *context) scan(name string) {
@@ -43,7 +47,31 @@ func (c context) Type() string {
 	return c.typeNode.Name.Name
 }
 
-func (c context) Fields() []field {
+func (c context) CoreFields() []field {
+	return c.getFields(true, false)
+}
+
+func (c context) ManagedFields() []field {
+	return c.getFields(false, true)
+}
+
+func (c context) AllFields() []field {
+	return c.getFields(true, true)
+}
+
+func (c context) SortedCoreFields() []field {
+	return c.sortFields(c.CoreFields())
+}
+
+func (c context) SortedManagedFields() []field {
+	return c.sortFields(c.ManagedFields())
+}
+
+func (c context) HasManagedVars() bool {
+	return len(c.ManagedFields()) > 0
+}
+
+func (c context) getFields(wantCore, wantManaged bool) []field {
 	structDef, ok := c.typeNode.Type.(*ast.StructType)
 	if !ok {
 		return nil
@@ -51,14 +79,16 @@ func (c context) Fields() []field {
 	fields := make([]field, 0, structDef.Fields.NumFields())
 	for _, fAST := range structDef.Fields.List {
 		for _, fN := range fAST.Names {
-			fields = append(fields, c.newField(fN.Name, fAST))
+			isManaged := strings.Contains(fN.Name, managedVarName)
+			if wantCore && !isManaged || wantManaged && isManaged {
+				fields = append(fields, c.newField(fN.Name, fAST))
+			}
 		}
 	}
 	return fields
 }
 
-func (c context) SortedFields() []field {
-	fields := c.Fields()
+func (c context) sortFields(fields []field) []field {
 	ptrspecifiers := make([]field, 0, len(fields))
 	for _, f := range fields {
 		if f.IsPointer() {
