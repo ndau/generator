b0f54cd3036d5522eccaaa2374f8673da844973b
Kent Quirk <kentquirk@gmail.com>
2019-10-04 17:01:33 -0400

    Add copyrights

--- pkg/txgen/generate.go
+++ pkg/txgen/generate.go
@@ -8,3 +8,12 @@ package txgen
 //go:generate rm -rf $GOPATH/src/github.com/oneiro-ndev/generator/cmd/maketests
 
 //go:generate find $GOPATH/src/github.com/oneiro-ndev/ndau/pkg/ndau/ -name "*_gen*.go" -maxdepth 1 -exec goimports -w {} ;
+
+// ----- ---- --- -- -
+// Copyright 2019 Oneiro NA, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License 2.0 (the "License").  You may not use
+// this file except in compliance with the License.  You can obtain a copy
+// in the file LICENSE in the source distribution or at
+// https://www.apache.org/licenses/LICENSE-2.0.txt
+// - -- --- ---- -----
